5755977712551eb904ac2c414b1126a96ddf0d31
Emmanuel Texier <emmanuel.texier@magnet.com>
2014-03-28 10:37:56 -0700

    New version of mab.rb installer including many checks.

--- mab.rb
+++ mab.rb
@@ -2,45 +2,178 @@ require "formula"
 
 JAVA_MIN_VERSION = "1.7.0_51"
 
-NO_JAVA_MESSAGE = """
-ERROR: No Java found. 
-"""
-
-INCORRECT_JAVA_MESSAGE = """
-ERROR: Incorrect Java version.
-  The Magnet Mobile App builder requires Oracle Java #{JAVA_MIN_VERSION} or above.
-"""
-
-class JavaOnPathRequirement < Requirement
-  fatal true
-
-  def message
-    NO_JAVA_MESSAGE
-  end
-
-  satisfy :build_env => false do
-    which 'java'
-  end
+def error_no_java
+  "ERROR: No Java executable found on PATH\n"
 end
 
-class JavaVersionRequirement < Requirement
+def error_no_java_in_java_home(java_home)
+"""
+ERROR: Cannot find Java executable under JAVA_HOME : #{java_home}/bin/java
+"""
+end
+
+def error_invalid_java_version (version) 
+"""  
+ERROR: Incorrect Java version #{version}.
+  The Magnet Mobile App builder requires Oracle Java #{JAVA_MIN_VERSION} or above.
+"""
+end
+
+def error_no_maven
+  "ERROR: No Maven executable found on PATH\n"
+end
+
+def error_cannot_found_maven_version 
+"""  
+ERROR: Cannot determine Maven version.
+  Verify that you have correctly set your JAVA_HOME or M2_HOME
+"""
+end
+
+def error_no_maven_on_maven_home(maven_home)
+"""
+ERROR: Incorrect M2_HOME : cannot find Maven executable at #{maven_home}/bin/mvn
+"""
+end
+
+def error_invalid_maven_version (version) 
+"""  
+ERROR: The installed Maven version is #{version}. 
+  It is recommended to use Apache Maven 3.1+.
+  You can install the latest version of maven with 'brew install maven'.
+  It is also recommended you remove the old version from your PATH.
+  Finally, if you have set M2_HOME, be sure it points to the right maven installation.
+"""
+end
+
+def error_no_java_home
+"""
+WARNING: You have not set a JAVA_HOME environment variable. 
+  It is recommended that you set it in order to ensure proper Maven behavior.
+"""
+end
+
+
+def error_maven_home_mismatch(maven_on_maven_home, maven_path)
+"""
+ERROR: Maven executables mismatch.
+  M2_HOME points to #{maven_on_maven_home} which is different from the 
+  Maven executable on the PATH: #{maven_path} 
+"""
+end
+
+def error_java_home_mismatch(java_on_java_home, java_path)
+"""
+ERROR: Java executables mismatch.
+  JAVA_HOME points to #{java_on_java_home} which is different from the 
+  Java executable on the PATH #{java_path} 
+"""
+end
+
+
+def check_java
+  
+  java = which 'java'
+  if not java 
+    return error_no_java
+  end
+  
+  version = /[0-9]+\.[0-9]+\.[\.0-9_]+/.match(`#{java} -version 2>&1| grep "java version"`).to_s
+  version_numbers = version.split('.')
+  if (version_numbers[1].to_i < 7) or (version_numbers[2].split('_')[1].to_i < 51)
+    return error_invalid_java_version(version)
+  end
+
+  nil
+end
+
+def check_java_home
+  java_home = ENV['JAVA_HOME']
+
+  if not java_home
+    return error_no_java_home
+  end 
+
+  begin
+    java_on_java_home = Pathname.new("#{java_home}/bin/java").realpath.to_s
+  rescue Errno::ENOENT
+    return error_no_java_in_java_home(java_home)
+  end
+
+  java = which 'java'
+
+  java_path = Pathname.new("#{java}").realpath.to_s
+  if java_on_java_home != java_path
+    return error_java_home_mismatch(java_on_java_home, java_path)
+  end
+  
+  nil
+end
+
+
+def check_maven
+  mvn = which 'mvn'
+  if not mvn 
+    return error_no_maven
+  end 
+
+  version =  /[0-9]+\.[0-9]+[\.0-9]*/.match(`#{mvn} -v | grep "Apache Maven"`).to_s
+  if version == ""
+    return error_cannot_found_maven_version
+  end 
+
+  version_numbers = version.split('.')
+  if not (version_numbers[0].to_i >= 3 and version_numbers[1].to_i >=1)
+    return error_invalid_maven_version(version)
+  end
+
+  maven_home = ENV['M2_HOME']
+
+  if not maven_home 
+    return nil
+  end
+
+  begin
+    maven_on_maven_home = Pathname.new("#{maven_home}/bin/mvn").realpath.to_s
+  rescue Errno::ENOENT
+    return error_no_maven_on_maven_home(maven_home)
+  end
+  
+  maven_path = Pathname.new("#{mvn}").realpath.to_s
+  if maven_path != maven_on_maven_home
+    return error_maven_home_mismatch(maven_on_maven_home, maven_path)
+  end
+
+  nil
+end
+
+class JavaDependency < Requirement
   fatal true
 
+  @error = nil
+
   def message
-    INCORRECT_JAVA_MESSAGE
+    @error
   end
 
   satisfy :build_env => false do
-    which 'java'
+    not (@error = check_java)
   end
+
 end
 
 class MavenDependency < Requirement
   fatal true
   default_formula 'maven'
- 
+
+  @error = nil
+
+  def message
+    @error
+  end
+
   satisfy do
-    which 'mvn'
+    not (@error = check_maven)
   end
 end
 
@@ -51,7 +184,6 @@ class MySqlDependency < Requirement
   satisfy do
    which 'mysql_config'
   end
-
 end
 
 #
@@ -73,8 +205,7 @@ class Mab < Formula
 
   depends_on MavenDependency => :recommended
   depends_on MySqlDependency => :recommended
-  depends_on JavaOnPathRequirement => :recommended
-  depends_on JavaVersionRequirement => :recommended
+  depends_on JavaDependency => :recommended
 
   
   def install
@@ -83,65 +214,10 @@ class Mab < Formula
 
 
   def caveats 
-
-    caveats = ""
-    
-    java = which 'java'
-    if not java
-      caveats = caveats + NO_JAVA_MESSAGE
-    else
-      version = /[0-9]+\.[0-9]+\.[\.0-9_]+/.match(`#{java} -version 2>&1| grep "java version"`).to_s
-      version_numbers = version.split('.')
-      if (version_numbers[1].to_i < 7)
-        caveats = caveats + INCORRECT_JAVA_MESSAGE
-      elsif (version_numbers[2].split('_')[1].to_i < 51)
-        wrong_java_patch_version = <<-EOS.undent
-        WARNING: Incorrect Java patch version. 
-          Current Java version is #{version}. 
-          The Magnet Mobile App builder requires Oracle Java #{JAVA_MIN_VERSION} or above.
-        EOS
-        caveats = caveats + wrong_java_patch_version
-      end
-    end
-
-
-    java_home = ENV['JAVA_HOME']
- 
-    if not java_home
-      no_java_home = <<-EOS.undent
-      WARNING: You have not set a JAVA_HOME environment variable. 
-        It is recommended that you set it in order to ensure proper Maven behavior.
-      EOS
-      caveats = caveats + no_java_home
-    else 
-      java_on_java_home = Pathname.new("#{java_home}/bin/java").realpath.to_s
-      java_path = Pathname.new("#{java}").realpath.to_s
-      if java_on_java_home != java_path
-        java_mismatch = <<-EOS.undent
-        ERROR: Java executables mismatch.
-          JAVA_HOME points to #{java_on_java_home} which is different from the 
-          Java executable on the PATH #{java_path} 
-        EOS
-        caveats = caveats + java_mismatch
-      end
-    end
-
-    mvn = which 'mvn'
-    version =  /[0-9]+\.[0-9]+[\.0-9]*/.match(`#{mvn} -v | grep "Apache Maven"`).to_s
-    version_numbers = version.split('.')
-    if not (version_numbers[0].to_i >= 3 and version_numbers[1].to_i >=1)
-      wrong_maven_version = <<-EOS.undent
-      WARNING: The installed Maven version is #{version}. 
-        It is recommended to use Apache Maven 3.1+.
-        You can install the latest version of maven with 'brew install maven', 
-        and remove the old version from your PATH.
-        Finally, if you have set M2_HOME, ensure that it is pointing to the right maven installation.
-      EOS
-      caveats = caveats + wrong_maven_version
-    end
-     
-    caveats
-
+    (check_java ? check_java : "") + (check_java_home ? check_java_home : "") + (check_maven ? check_maven : ""))
   end 
+
+  test do
+  end
 
 end
